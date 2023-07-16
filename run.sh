7a3b3b7f5d43890492d3b45e0395b3381138d5a6
Aagaman Luitel <mail@luitelaagaman.com.np>
2023-07-16 15:54:23 +0545

    binary name = bancharo
    and not a.out

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-gcc src/ast.c src/hashmap.c src/codegen.c src/bancharo.c src/parser.c src/scanner.c -g && ./a.out test
+gcc src/ast.c src/hashmap.c src/codegen.c src/bancharo.c src/parser.c src/scanner.c -g -o bancharo && ./bancharo test
